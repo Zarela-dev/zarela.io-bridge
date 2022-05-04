6e1b468d85fb0adbf0a4353aca043f0015e57d6c
Hoss Azadi <azadi.hoss@gmail.com>
2022-05-05 02:19:08 +0430

    migrate packages and config from zarela.io-interface
    (cherry picked from commit e68dd20c5788ddaed8d682bf38ef463e23d5dc3e)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  images: {
+    domains: ['localhost', 'zarela.io', 'admin-api.zarela.io'],
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  async redirects() {
+    return [
+      {
+        source: '/metaverse',
+        destination: '/bioverse',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
-
-module.exports = nextConfig
