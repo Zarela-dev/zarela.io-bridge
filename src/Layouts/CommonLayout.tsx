494af0ad4e222bb5b775631a9a2aeb10f2386a1c
Hoss Azadi <azadi.hoss@gmail.com>
2022-05-07 15:24:55 +0430

    add Header to CommonLayout
    (cherry picked from commit 1abce8df3fcb58cbe6c9326df3501e5e461636ff)

--- src/Layouts/CommonLayout.tsx
+++ src/Layouts/CommonLayout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react'
 import { ThemeProvider } from '../Theme'
 import Head from 'next/head'
+import Header from '../components/Header'
+import { Box } from 'rebass/styled-components'
 
 export default function CommonLayout({ children }: { children: ReactNode }) {
   return (
@@ -11,7 +13,10 @@ export default function CommonLayout({ children }: { children: ReactNode }) {
           content="width=device-width, minimum-scale=1.0, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
       </Head>
-      <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        <Header />
+        <Box>{children}</Box>
+      </ThemeProvider>
     </>
   )
 }
